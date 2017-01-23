23ecc5ca869262dcceb07cacd0931d65ef04ab6e
AJ <42003122+code7885@users.noreply.github.com>
2017-01-23 20:23:56 +0100

    solving problem of react-router render for AddItemFrom

--- public_html/_/components/js/modules/ui/AddItemForm.js
+++ public_html/_/components/js/modules/ui/AddItemForm.js
@@ -1,7 +1,7 @@
-import React,{ PropTypes } from 'react'
+import { PropTypes } from 'react'
 
-const AddItemForm = ({ onNewItem=f=>f, router}) => {
-//const AddItemForm = () => {
+export const AddItemForm = ({ onNewItem=f=>f, router}) => {
+
     let _itemName
 
     const submit = e => {
@@ -11,12 +11,12 @@ const AddItemForm = ({ onNewItem=f=>f, router}) => {
             itemCount: 1
         })
 
-        router.push('/')
+       router.push('/')
         _itemName.value = ''
     }
 
     return (
-        <form onSubmit={submit} >something
+        <form onSubmit={submit} >
             <label htmlFor="item-name"> Name Item</label>
             <input id="item-name" type="text" ref={(input) => _itemName = input } />     
             <button>Add Item</button>
@@ -27,13 +27,3 @@ AddItemForm.propTypes = {
     onNewItem: PropTypes.func,
     router: PropTypes.object
 }
-
-export default AddItemForm
-
-/*export const AddItemForm = () =>
-     <form onSubmit={submit} >something
-            <label htmlFor="item-name"> Name Item</label>
-            <input id="item-name" type="text"  />     
-            <button>Add Item</button>
-        </form>
-*/
