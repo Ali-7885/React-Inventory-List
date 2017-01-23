2a5f09eec65041a2abc1548b5ebe5c4115f36f97
AJ <42003122+code7885@users.noreply.github.com>
2017-01-23 14:20:12 +0100

    add redux to the project

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,29 @@
 var webpack = require("webpack");
-
+var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
     entry: './public_html/_/components/js/app.js',
     output: {
         path: './public_html/_/js',
         filename: 'bundle.js',
-        publicPath: 'js'
+        publicPath: 'public_html'
     },
+    devtool: 'source-map',
     devServer: {
         inline: true,
-        contentBase: "public_html",
+        contentBase: "./public_html",
         port: 3000
     },
     module: {
         loaders: [
-            {
+            { 
+                test: /\.vue$/, 
+                loader: 'vue'
+            },
+            {   
                 test: /\.js$/,
                 exclude: /(node_modules)/,
-                loader: ["babel-loader"],
+                loader: ["babel"],
                 query: {
                     presets: ["latest", "stage-0", "react"]
                 }
@@ -33,9 +38,17 @@ module.exports = {
                 loader: 'style-loader!css-loader!autoprefixer-loader'
             },
             {
-                test: /\.scss$/,
+                test: /\.scss/,
                 loader: 'style-loader!css-loader!autoprefixer-loader!sass-loader'
             }
         ]
-    }
-};
+    },
+    plugins: [
+        new OptimizeCssAssetsPlugin({
+            assetNameRegExp: /\.optimize\.css$/g,
+            cssProcessor: require('cssnano'),
+            cssProcessorOptions: {discardComments: {removeAll: true}},
+            canPrint: true
+        })
+    ]
+}
