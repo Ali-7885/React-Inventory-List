3c9914b058104812366d2fd1f3fad3092d27e9a3
AJ <42003122+code7885@users.noreply.github.com>
2017-01-25 04:37:15 +0100

    add item to the list

--- _/components/js/modules/containers/AddItemForm.js
+++ _/components/js/modules/containers/AddItemForm.js
@@ -3,22 +3,20 @@ import { withRouter } from 'react-router'
 import { connect } from 'react-redux'
 import { addItem  } from '../../actions'
 
-const mapStateToProps = (state, props) => 
+const mapStateToProps = (state, props) =>
   ({
     items:  state.items
   })
 
-const mapDispatchToProps = (disptach) => 
-  ({  
-    onAddItem(itemName) {    
+const mapDispatchToProps = (disptach) =>
+  ({
+    onAddItem(itemName) {
             console.log(itemName);
             disptach(
                  addItem(itemName)
             )
-//      document.location.href = '/'
     }
-})  
+})
 
-export default connect(mapStateToProps,mapDispatchToProps)(AddItemForm) 
-    
-export default withRouter(Container)
+export default connect(mapStateToProps,mapDispatchToProps)(AddItemForm)
+// export default withRouter(Container)
