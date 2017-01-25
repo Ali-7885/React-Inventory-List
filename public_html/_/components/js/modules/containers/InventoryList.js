522ead77badbb01dc30fd1ed17bae1987c573448
AJ <42003122+code7885@users.noreply.github.com>
2017-01-25 04:37:15 +0100

    add item to the list

--- public_html/_/components/js/modules/containers/InventoryList.js
+++ public_html/_/components/js/modules/containers/InventoryList.js
@@ -3,39 +3,39 @@ import { connect } from 'react-redux'
 import { addItem , removeItem } from '../../actions'
 import { PropTypes } from 'react'
 
-const mapStateToProps = (state, props) => 
+const mapStateToProps = (state, props) =>
   ({
    items:  state.items,
    selectedItems: state.selectedItems
-   
+
   })
 
-const mapDispatchToProps = (disptach) => 
-  ({  
+const mapDispatchToProps = (disptach) =>
+  ({
     onSelectItem(itemName) {
         console.log('onSelectItem - - '+itemName)
 //        store.getState().selectedItems.has(itemName.trim())?
-        
+
     },
-    
+
     onAddItem( ) {
         if (store.getState().selectedItems.length<1){
 
         document.location.href = '/#/add'
         }else{
             disptach(
-                 addItem('something new')
+                 addItem('Ali')
             )
         }
     },
-    
+
     onRemoveItem(itemName) {
         console.log('onRemoveItem')
         disptach(
-            removeItem('something new')
+            removeItem('Ali')
         )
     }
- 
-  })  
 
-export default connect(mapStateToProps,mapDispatchToProps)(InventoryList)  
+  })
+
+export default connect(mapStateToProps,mapDispatchToProps)(InventoryList)
