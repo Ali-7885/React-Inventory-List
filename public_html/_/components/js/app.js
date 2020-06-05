d83ed91c86094ae72d407828f2390e04baa30722
AJ <42003122+code7885@users.noreply.github.com>
2020-06-05 23:35:25 +0200

    Initial public_html subtree commit

--- public_html/_/components/js/app.js
+++ public_html/_/components/js/app.js
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright 2017 MAVAJ SUN CO.
+ * Copyright 2017-2020 WebChannel.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
