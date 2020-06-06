77bc7d55fa8d917bb580d56be753bda1cf6d7dc5
AJ <42003122+code7885@users.noreply.github.com>
2020-06-06 08:13:10 +0000

    fix the path issue

--- _/components/js/modules/PageNotFound.js
+++
@@ -1,9 +0,0 @@
-export const PageNotFound = ({ location }) =>
-<div className="container">
-  <div className="row">
-    <div className="col-sm-6 col-sm-offset-3 col-xs-12">
-        <h1>Oops ! - The page has not found !</h1>
-        <p>Cannot find content for {location.pathname}</p>
-    </div>
-  </div>
-</div>
